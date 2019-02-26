f6e959e2e9c09a99b380a4627c635a9a20808824
Jonas Schubert <guepardoapps@gmail.com>
2019-02-26 20:52:54 +0100

    restructured and restarted flutter project

---
+++ lucahome_flutter/lib/models/area.model.dart
@@ -0,0 +1,5 @@
+class Area {
+  String name;
+  String filter;
+  int deletable = 1;
+}
---
+++ lucahome_flutter/lib/models/wireless_socket.model.dart
@@ -0,0 +1,9 @@
+class WirelessSocket {
+  String name;
+  String code;
+  String area;
+  int state = 0;
+  String description;
+  String icon;
+  int deletable = 1;
+}
