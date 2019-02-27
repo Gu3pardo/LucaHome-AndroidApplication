18e6560d50882fd82c2c90bb773172f291d94b10
Jonas Schubert <guepardoapps@gmail.com>
2019-02-27 21:33:52 +0100

    Update routes.dart

--- lucahome_flutter/lib/routes.dart
+++ lucahome_flutter/lib/routes.dart
@@ -3,11 +3,10 @@ library routes;
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:lucahome_flutter/models/app_state.model.dart';
-import 'package:lucahome_flutter/models/wireless_socket.model.dart';
-import 'package:lucahome_flutter/pages/login_page.dart';
-import 'package:lucahome_flutter/pages/loading_page.dart';
-import 'package:lucahome_flutter/pages/list_page.dart';
-import 'package:lucahome_flutter/pages/details_page.dart';
+import 'package:lucahome_flutter/pages/login.page.dart';
+import 'package:lucahome_flutter/pages/loading.page.dart';
+import 'package:lucahome_flutter/pages/list.page.dart';
+import 'package:lucahome_flutter/pages/details.page.dart';
 
 void updateRoute(AppState state, BuildContext context) {
   if (state.nextCloudCredentials != null) {
@@ -19,12 +18,12 @@ Map<String, WidgetBuilder> getRoutes(context, store) {
   return {
     '/': (BuildContext context) => new StoreBuilder<AppState>(
           builder: (context, store) {
-            return new ListPage();
+            return new ListPage(store.state.wirelessSocketList);
           },
         ),
     '/details': (BuildContext context) => new StoreBuilder<AppState>(
           builder: (context, store) {
-            return new DetailsPage(/* TODO Add WirelssSocket */);
+            return new DetailsPage(store.state.selectedWirelessSocket);
           },
         ),
     '/login': (BuildContext context) => new StoreBuilder<AppState>(
