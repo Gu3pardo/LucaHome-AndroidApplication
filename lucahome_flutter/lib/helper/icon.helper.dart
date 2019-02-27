df5a1577a1d9e37eee5eca41b8a4d8fe58972de5
Jonas Schubert <guepardoapps@gmail.com>
2019-02-27 21:14:20 +0100

    Update icon.helper.dart

--- lucahome_flutter/lib/helper/icon.helper.dart
+++ lucahome_flutter/lib/helper/icon.helper.dart
@@ -2,11 +2,9 @@ import 'package:flutter/widgets.dart';
 import 'package:lucahome_flutter/constants/font_awesome.constants.dart';
 import 'package:lucahome_flutter/helper/string.helper.dart';
 
-class IconHelper {
-  IconData fromString(String icon) {
-    return FontAwesomeConstants()
-        .icons
-        .singleWhere((x) => x.key == new StringHelper(icon.substring(icon.indexOf("-"))).camelCase)
-        .value;
-  }
+IconData fromString(String icon) {
+  return FontAwesomeConstants()
+      .icons
+      .singleWhere((x) => x.key == new StringHelper(icon.substring(icon.indexOf("-"))).camelCase)
+      .value;
 }
