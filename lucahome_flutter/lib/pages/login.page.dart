608e1e3b8a862c6ddbf2259ae1cb397ef9813bae
Jonas Schubert <guepardoapps@gmail.com>
2019-02-28 21:28:25 +0100

    adds form to login page, adds validator, changed login page style

--- lucahome_flutter/lib/pages/login.page.dart
+++ lucahome_flutter/lib/pages/login.page.dart
@@ -8,8 +8,12 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    var pageSize = MediaQuery.of(context).size;
+
     final logo = Hero(
       tag: 'hero',
       child: CircleAvatar(
@@ -27,7 +31,15 @@ class _LoginPageState extends State<LoginPage> {
         hintText: 'NextCloudUrl',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        hintStyle: TextStyle(color: Colors.white54),
+        errorStyle: TextStyle(color: Colors.red),
       ),
+      style: TextStyle(color: Colors.white),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'NextCloudUrl is required';
+        }
+      },
     );
 
     final userName = TextFormField(
@@ -38,7 +50,15 @@ class _LoginPageState extends State<LoginPage> {
         hintText: 'UserName',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        hintStyle: TextStyle(color: Colors.white54),
+        errorStyle: TextStyle(color: Colors.red),
       ),
+      style: TextStyle(color: Colors.white),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'UserName is required';
+        }
+      },
     );
 
     final passPhrase = TextFormField(
@@ -49,7 +69,15 @@ class _LoginPageState extends State<LoginPage> {
         hintText: 'Password',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        hintStyle: TextStyle(color: Colors.white54),
+        errorStyle: TextStyle(color: Colors.red),
       ),
+      style: TextStyle(color: Colors.white),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Password is required';
+        }
+      },
     );
 
     final loginButton = Padding(
@@ -59,12 +87,14 @@ class _LoginPageState extends State<LoginPage> {
           borderRadius: BorderRadius.circular(24),
         ),
         onPressed: () {
-          // TODO Do something with the data
-          var route = new MaterialPageRoute(
-              settings: new RouteSettings(name: '/login'),
-              builder: (context) => new LoginPage());
-          Navigator.of(context)
-              .pushAndRemoveUntil(route, ModalRoute.withName('/loading'));
+          if (_formKey.currentState.validate()) {
+            // TODO Do something with the data
+            var route = new MaterialPageRoute(
+                settings: new RouteSettings(name: '/login'),
+                builder: (context) => new LoginPage());
+            Navigator.of(context)
+                .pushAndRemoveUntil(route, ModalRoute.withName('/loading'));
+          }
         },
         padding: EdgeInsets.all(12),
         color: Colors.lightBlueAccent,
@@ -72,25 +102,44 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            logo,
-            SizedBox(height: 48.0),
-            nextCloudUrl,
-            SizedBox(height: 24.0),
-            userName,
-            SizedBox(height: 8.0),
-            passPhrase,
-            SizedBox(height: 24.0),
-            loginButton
-          ],
-        ),
-      ),
-    );
+    return Form(
+        key: _formKey,
+        child: new Scaffold(
+          body: new Stack(
+            children: <Widget>[
+              new Container(
+                alignment: Alignment.center,
+                width: pageSize.width,
+                height: pageSize.height,
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    stops: [0.2, 1.0],
+                    colors: [
+                      const Color(0xFF3744B0),
+                      const Color(0xFF3799B0),
+                    ],
+                  ),
+                ),
+                child: ListView(
+                  shrinkWrap: true,
+                  padding: EdgeInsets.only(left: 24.0, right: 24.0),
+                  children: <Widget>[
+                    logo,
+                    SizedBox(height: 48.0),
+                    nextCloudUrl,
+                    SizedBox(height: 24.0),
+                    userName,
+                    SizedBox(height: 8.0),
+                    passPhrase,
+                    SizedBox(height: 24.0),
+                    loginButton
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
