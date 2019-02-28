e7bf311b3fa7f7752e818908549c55efdac73c9d
Jonas Schubert <guepardoapps@gmail.com>
2019-02-28 21:54:22 +0100

    adds style to list page

--- lucahome_flutter/lib/pages/list.page.dart
+++ lucahome_flutter/lib/pages/list.page.dart
@@ -10,12 +10,37 @@ class ListPage extends StatelessWidget {
   ListView _buildList(context) {
     return new ListView.builder(
       itemCount: wirelessSocketList.length,
-      itemBuilder: (context, index) => new WirelessSocketCard(wirelessSocketList[index]),
+      itemBuilder: (context, index) =>
+          new WirelessSocketCard(wirelessSocketList[index]),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return _buildList(context);
+    var pageSize = MediaQuery.of(context).size;
+
+    return new Stack(
+      children: <Widget>[
+        new Container(
+          alignment: Alignment.center,
+          width: pageSize.width,
+          height: pageSize.height,
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.2, 1.0],
+              colors: [
+                const Color(0xFF3744B0),
+                const Color(0xFF3799B0),
+              ],
+            ),
+          ),
+          child: new Center(
+            child: _buildList(context),
+          ),
+        ),
+      ],
+    );
   }
 }
