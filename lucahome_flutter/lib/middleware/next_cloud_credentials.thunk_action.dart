e62da80117a68fcd7ac18b2ccac9e18135553463
Jonas Schubert <guepardoapps@gmail.com>
2019-03-02 09:20:37 +0100

    Handling error status codes

--- lucahome_flutter/lib/middleware/next_cloud_credentials.thunk_action.dart
+++ lucahome_flutter/lib/middleware/next_cloud_credentials.thunk_action.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:lucahome_flutter/actions/next_cloud_credentials.actions.dart';
 import 'package:lucahome_flutter/constants/nextcloud.constants.dart';
+import 'package:lucahome_flutter/models/api_response.model.dart';
 import 'package:lucahome_flutter/models/app_state.model.dart';
 import 'package:lucahome_flutter/models/next_cloud_credentials.model.dart';
 import 'package:redux/redux.dart';
@@ -9,16 +10,39 @@ import 'package:redux_thunk/redux_thunk.dart';
 
 ThunkAction<AppState> logIn(NextCloudCredentials nextCloudCredentials) {
   return (Store<AppState> store) async {
-    var authorization = 'Basic ' + base64Encode(utf8.encode('${nextCloudCredentials.userName}:${nextCloudCredentials.passPhrase}'));
+    var authorization = 'Basic ' +
+        base64Encode(utf8.encode(
+            '${nextCloudCredentials.userName}:${nextCloudCredentials.passPhrase}'));
 
     var response = await http.get(
         nextCloudCredentials.baseUrl + NextCloudConstants.baseUrl + 'area',
         headers: {'authorization': authorization});
 
-    if (response.statusCode == 200 && jsonDecode(response.body)["status"] == "success") {
-      store.dispatch(new NextCloudCredentialsLogInSuccessful(user: nextCloudCredentials));
-    } else {
-      store.dispatch(new NextCloudCredentialsLogInFail(response.statusCode));
+    switch (response.statusCode) {
+      // 404 For invalid URL
+      case 404:
+        store.dispatch(new NextCloudCredentialsLogInFail("Invalid URL"));
+        break;
+
+      // 401 For invalid userName with message: CORS requires basic auth
+      // 401 For invalid passPhrase with message: CORS requires basic auth
+      case 401:
+        store.dispatch(new NextCloudCredentialsLogInFail("Invalid Credentials"));
+        break;
+
+      // Valid
+      case 200:
+        var apiResponseModel = new ApiResponseModel.fromJson(jsonDecode(response.body));
+        if (apiResponseModel.status == "success") {
+          store.dispatch(new NextCloudCredentialsLogInSuccessful(user: nextCloudCredentials));
+        } else {
+          store.dispatch(new NextCloudCredentialsLogInFail(apiResponseModel.status));
+        }
+        break;
+
+      default:
+        store.dispatch(new NextCloudCredentialsLogInFail("Unknown error"));
+        break;
     }
   };
 }
