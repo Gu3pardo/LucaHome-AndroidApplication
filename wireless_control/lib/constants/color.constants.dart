6fc5890f3ced6fc3edcbbe1fb0ddb0ab1d93fbe8
Jonas Schubert <guepardoapps@gmail.com>
2019-03-13 19:31:42 +0100

    Create color.constants.dart

---
+++ wireless_control/lib/constants/color.constants.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const AppBar = Color(0xFF3799B0);
+
+  static const BackgroundDark = Colors.black54;
+  static const BackgroundLight = Colors.white;
+  static const BackgroundGradient = [Color(0xFF3744B0), Color(0xFF3799B0)];
+
+  static const ButtonSubmit = Colors.lightBlueAccent;
+  static const ButtonSubmitHighlight = Colors.blueAccent;
+  static const ButtonDelete = Colors.redAccent;
+
+  static const CardBackgroundDarkTransparent = Colors.black54;
+  static const CardBackgroundDark = Colors.black87;
+  static const CardBackgroundLightTransparent = Colors.white70;
+  static const CardBackgroundLight = Colors.white;
+
+  static const IconDark = Colors.black87;
+  static const IconLight = Colors.white;
+
+  static const Error = Colors.red;
+  static const TextDark = Colors.black87;
+  static const TextLight = Colors.white;
+  static const Hint = Colors.white54;
+
+  static const WaveOff = [
+    [Colors.red, Color(0xEEF44336)],
+    [Color(0x88FF0000), Color(0x77E57373)],
+    [Colors.orange, Color(0x66FF9800)],
+    [Colors.yellow, Color(0x55FFEB3B)]
+  ];
+  static const WaveOn = [
+    [Colors.yellow, Color(0x55FFEB3B)],
+    [Colors.orange, Color(0x66FF9800)],
+    [Color(0x8800FF00), Color(0x77007373)],
+    [Colors.green, Color(0xEE004336)]
+  ];
+}
